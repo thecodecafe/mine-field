c64e3aa92da0992ce324488af1732adb946753fa
Daniel Barde <mrbarde2k@gmail.com>
2019-01-17 20:34:37 +0100

    Some updates

---
+++ assets/js/class.js
@@ -0,0 +1,16 @@
+// LET
+// 1. Let scoped, it does not affect other variables outside of it's scope.
+// 2. Let does not allow redeclaration of variables.
+
+// CONST
+// 
+
+name = "Daniel";
+color = "Gray"
+
+const person = {
+	name: "Daniel",
+	color: "Grey"
+};
+
+person.name = "";
